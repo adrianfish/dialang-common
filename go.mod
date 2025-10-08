9bb31a8f7647ed47ec297ffbf36bbdee1bc3f075
Adrian Fish <adrian.r.fish@gmail.com>
2025-10-08 19:44:38 +0100

    Initial

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/dialangproject/common
+
+go 1.25.0
+
+require (
+	github.com/lib/pq v1.10.9
+	github.com/magiconair/properties v1.8.10
+)
---
+++ models/models.go
@@ -0,0 +1,128 @@
+package models
+
+import (
+	"html/template"
+)
+
+type AdminLanguage struct {
+	Locale      string
+	Description string
+}
+
+type TestLanguage struct {
+	Locale      string
+	TwoLetterCode string
+}
+
+type SAWeight struct {
+	Skill string
+	WordId string
+	Weight int
+}
+
+type Basket struct {
+	Id int
+	Type string
+	ParentTestletId int
+	ParentTestletPosition int
+	Skill string
+	Label string
+	Prompt string
+	GapText string
+	Weight int
+	MediaType string
+	TextMedia string
+	FileMedia string
+}
+
+type Item struct {
+	Id int `json:"id"`
+	Type string `json:"type"`
+	Skill string `json:"skill"`
+	Position int `json:"position"`
+	SubSkill string `json:"subskill"`
+	Text template.HTML `json:"text"`
+	Weight int `json:"weight"`
+}
+
+type Answer struct {
+	Id int `json:"id"`
+	ItemId int `json:"itemId"`
+	Text string `json:"text"`
+	Correct int `json:"correct"`
+}
+
+type SAStatement struct {
+	Locale string
+	Skill string
+	WordId string
+	Statement string
+}
+
+type SetTLParams struct {
+	SessionId string `json:"sessionId"`
+	PassId string `json:"passId"`
+	Al string `json:"al"`
+	Tl string `json:"tl"`
+	Skill string `json:"skill"`
+	IPAddress string
+	BrowserLocale string
+	Referrer string
+}
+
+type VSPTBand struct {
+	Locale string
+	Level string
+	Low int
+	High int
+}
+
+type VSPTWord struct {
+	WordId string
+	Word string
+	Valid int
+	Weight int
+}
+
+type SAGrade struct {
+	Skill string
+	Rsc int
+	Ppe float64
+	Se float64
+	Grade int
+}
+
+type PreestWeight struct {
+	Sa float64
+	Vspt float64
+	Coe float64
+}
+
+type PreestAssignment struct {
+	Pe float64
+	BookletId int
+}
+
+type Session struct {
+	Al       string `json:"al"`
+	Tl       string `json:"tl"`
+	Skill    string `json:"skill"`
+	VsptDone struct {
+		FraFr bool `json:"fra_fr"`
+	} `json:"vsptDone"`
+	ReviewBasket   any `json:"reviewBasket"`
+	ReviewItemID   any `json:"reviewItemId"`
+	FeedbackMode   bool        `json:"feedbackMode"`
+	TestDone       bool        `json:"testDone"`
+	TestDifficulty string      `json:"testDifficulty"`
+	Id             string      `json:"id"`
+	VsptSubmitted  int         `json:"vsptSubmitted"`
+	VsptMearaScore float64     `json:"vsptMearaScore"`
+	VsptZScore     float64     `json:"vsptZScore"`
+	VsptLevel      string      `json:"vsptLevel"`
+	SaSubmitted    int         `json:"saSubmitted"`
+	SaPPE          float64     `json:"saPPE"`
+	SaLevel        int         `json:"saLevel"`
+	SaDone         bool        `json:"saDone"`
+	BookletId	   int		   `json:"bookletId"`
+}
