bb7512543e16fc1ab29ae066c5869cb7ff53230e
Adrian Fish <adrian.r.fish@gmail.com>
2025-10-25 13:38:42 +0100

    ssafety

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/dialangproject/common
 
 go 1.25.0
-
-require (
-	github.com/lib/pq v1.10.9
-	github.com/magiconair/properties v1.8.10
-)
--- models/models.go
+++ models/models.go
@@ -14,6 +14,26 @@ type TestLanguage struct {
 	TwoLetterCode string
 }
 
+
+type VSPTBand struct {
+	Locale string
+	Level string
+	Low int
+	High int
+}
+
+type PreestWeight struct {
+	Sa float64
+	Vspt float64
+	Coe float64
+}
+
+type PreestAssignment struct {
+	Pe float64
+	BookletId int
+}
+
+
 type SAWeight struct {
 	Skill string
 	WordId string
@@ -69,24 +89,6 @@ type SAStatement struct {
 	Statement string
 }
 
-type SetTLParams struct {
-	SessionId string `json:"sessionId"`
-	PassId string `json:"passId"`
-	Al string `json:"al"`
-	Tl string `json:"tl"`
-	Skill string `json:"skill"`
-	IPAddress string
-	BrowserLocale string
-	Referrer string
-}
-
-type VSPTBand struct {
-	Locale string
-	Level string
-	Low int
-	High int
-}
-
 type VSPTWord struct {
 	WordId string
 	Word string
@@ -100,39 +102,4 @@ type SAGrade struct {
 	Ppe float64
 	Se float64
 	Grade int
-}
-
-type PreestWeight struct {
-	Sa float64
-	Vspt float64
-	Coe float64
-}
-
-type PreestAssignment struct {
-	Pe float64
-	BookletId int
-}
-
-type Session struct {
-	Al       string `json:"al"`
-	Tl       string `json:"tl"`
-	Skill    string `json:"skill"`
-	VsptDone struct {
-		FraFr bool `json:"fra_fr"`
-	} `json:"vsptDone"`
-	ReviewBasket   any `json:"reviewBasket"`
-	ReviewItemID   any `json:"reviewItemId"`
-	FeedbackMode   bool        `json:"feedbackMode"`
-	TestDone       bool        `json:"testDone"`
-	TestDifficulty string      `json:"testDifficulty"`
-	Id             string      `json:"id"`
-	VsptSubmitted  int         `json:"vsptSubmitted"`
-	VsptMearaScore float64     `json:"vsptMearaScore"`
-	VsptZScore     float64     `json:"vsptZScore"`
-	VsptLevel      string      `json:"vsptLevel"`
-	SaSubmitted    int         `json:"saSubmitted"`
-	SaPPE          float64     `json:"saPPE"`
-	SaLevel        int         `json:"saLevel"`
-	SaDone         bool        `json:"saDone"`
-	BookletId	   int		   `json:"bookletId"`
 }
